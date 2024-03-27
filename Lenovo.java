497ac906c2cf84bd70c43ea97316621b68061adc
RivaldySultan <115138860+RivaldySultan@users.noreply.github.com>
2024-03-27 16:25:59 +0700

    Add files via upload

---
+++ Lenovo.java
@@ -0,0 +1,71 @@
+public class Lenovo implements Laptop {
+    private int volume;
+    boolean is_power_on;
+    private boolean is_power_on;
+
+    public Lenovo() {
+        this.volume = 50;
+    }
+
+    @Override
+    public void powerOn() {
+        is_power_on = true;
+        System.out.println("Laptop is on");
+        System.out.println("Lenovo ThinkPad");
+    }
+
+    @Override
+    public void powerOff() {
+        is_power_on = false;
+        System.out.println("Shutdown is process....");
+
+    }
+
+    @Override
+    public void volumeUp() {
+        if (is_power_on) {
+            if (this.volume == MAX_VOL) {
+                System.out.println("Volume is full");
+            } else {
+                this.volume += 10;
+                System.out.println("Volume is :" + this.getVolume());
+            }
+        }
+    }
+
+    @Override
+    public void volumeDown() {
+        if (is_power_on) {
+            if (this.volume == MIN_VOL) {
+                System.out.println("Volume is 0%");
+            } else {
+                this.volume -= 10;
+                System.out.println("Volume is :" + this.getVolume());
+            }
+        }
+    }
+
+    public int getVolume() {
+        return this.volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public boolean isIs_power_on() {
+        return is_power_on;
+    }
+
+    public void setIs_power_on(boolean is_power_on) {
+        this.is_power_on = is_power_on;
+    }
+
+    public boolean isIs_power_on() {
+        return is_power_on;
+    }
+
+    public void setIs_power_on(boolean is_power_on) {
+        this.is_power_on = is_power_on;
+    }
+}
